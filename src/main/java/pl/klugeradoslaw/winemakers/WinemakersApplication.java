e623fd339450f6fabc9b579de2e8279dec48bacf
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-04-16 18:39:12 +0200

    project starte, added dev profile

---
+++ src/main/java/pl/klugeradoslaw/winemakers/WinemakersApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.winemakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinemakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinemakersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/klugeradoslaw/winemakers/WinemakersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.winemakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinemakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
