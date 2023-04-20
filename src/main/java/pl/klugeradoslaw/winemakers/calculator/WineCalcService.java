56a83394d8ca9e79b9cf8c7e38e6d9e4344fcc21
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-04-20 20:38:22 +0200

    added step 0.01 and result(amounOfSugar) now is rounded up to 2 decimal places

--- src/main/java/pl/klugeradoslaw/winemakers/calculator/WineCalcService.java
+++ src/main/java/pl/klugeradoslaw/winemakers/calculator/WineCalcService.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Service;
 public class WineCalcService {
 
     // 17 grams sugar in 1 liter of wine makes 1% alkohol
+    // rounded up to 2 decimal places -> (a * 100) / 100
     public double amountOfSugar(double alcoholPercentage, double amountOfWineInCarboy) {
-        return 17 * alcoholPercentage * amountOfWineInCarboy / 1000;
+        return Math.round((17 * alcoholPercentage * amountOfWineInCarboy / 1000)*100)/100D;
     }
 }
