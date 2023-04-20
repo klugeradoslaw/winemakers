2c5a95394495efe102c22bcf60441ad04b2346ac
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-04-20 20:12:17 +0200

    added comment in WinceCalcService to explain formula

--- src/main/java/pl/klugeradoslaw/winemakers/calculator/WineCalcService.java
+++ src/main/java/pl/klugeradoslaw/winemakers/calculator/WineCalcService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WineCalcService {
+
+    // 17 grams sugar in 1 liter of wine makes 1% alkohol
     public double amountOfSugar(double alcoholPercentage, double amountOfWineInCarboy) {
         return 17 * alcoholPercentage * amountOfWineInCarboy / 1000;
     }
