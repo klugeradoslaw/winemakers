babc9e335e70ad24f43149bc531d3b9e8813a448
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-05-03 17:56:09 +0200

    checkpoint

--- src/main/java/pl/klugeradoslaw/winemakers/step/StepRepository.java
+++ src/main/java/pl/klugeradoslaw/winemakers/step/StepRepository.java
@@ -3,6 +3,9 @@ package pl.klugeradoslaw.winemakers.step;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StepRepository extends JpaRepository<Step, Long> {
+
 }
--- src/main/java/pl/klugeradoslaw/winemakers/step/StepService.java
+++ src/main/java/pl/klugeradoslaw/winemakers/step/StepService.java
@@ -1,9 +1,24 @@
 package pl.klugeradoslaw.winemakers.step;
 
 import org.springframework.stereotype.Service;
+import pl.klugeradoslaw.winemakers.step.dto.StepFullResponseDto;
+
+import java.util.List;
 
 @Service
 public class StepService {
 
+    private final StepRepository stepRepository;
+
+    public StepService(StepRepository stepRepository) {
+        this.stepRepository = stepRepository;
+    }
+
+    public List<StepFullResponseDto> findAllStepsByWineId(long id) {
+        return stepRepository.findAll()
+                .stream()
+                .map(StepDtoMapper::mapFullResponse)
+                .toList();
+    }
 
 }
