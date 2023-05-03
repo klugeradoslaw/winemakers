9dde1fa91c85901571f13a1ae36e9c2afb9bd4a3
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-05-03 18:21:09 +0200

    added list of steps in view

--- src/main/java/pl/klugeradoslaw/winemakers/step/dto/StepFullResponseDto.java
+++ src/main/java/pl/klugeradoslaw/winemakers/step/dto/StepFullResponseDto.java
@@ -12,4 +12,28 @@ public class StepFullResponseDto {
         this.dateOfStep = dateOfStep;
         this.description = description;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateOfStep() {
+        return dateOfStep;
+    }
+
+    public void setDateOfStep(LocalDate dateOfStep) {
+        this.dateOfStep = dateOfStep;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
