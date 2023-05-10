5515b55d9948f6d805852f4af669e190716a605e
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-05-10 21:28:56 +0200

    added FileStorageService, method 'addWine'  in WineService

--- src/main/java/pl/klugeradoslaw/winemakers/wine/WineService.java
+++ src/main/java/pl/klugeradoslaw/winemakers/wine/WineService.java
@@ -3,6 +3,7 @@ package pl.klugeradoslaw.winemakers.wine;
 import org.springframework.stereotype.Service;
 import pl.klugeradoslaw.winemakers.wine.dto.WineFullResponseDto;
 import pl.klugeradoslaw.winemakers.wine.dto.WineHomePageDto;
+import pl.klugeradoslaw.winemakers.wine.dto.WineSaveDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,8 @@ public interface WineService {
 
     public List<WineHomePageDto> findAll();
     public Optional<WineFullResponseDto> findWineById(long id);
+
+    public void addWine(WineSaveDto wineToSave);
 
 
 }
--- src/main/java/pl/klugeradoslaw/winemakers/wine/WineServiceImpl.java
+++ src/main/java/pl/klugeradoslaw/winemakers/wine/WineServiceImpl.java
@@ -1,18 +1,24 @@
 package pl.klugeradoslaw.winemakers.wine;
 
 import org.springframework.stereotype.Service;
+import pl.klugeradoslaw.winemakers.step.Step;
 import pl.klugeradoslaw.winemakers.wine.dto.WineFullResponseDto;
 import pl.klugeradoslaw.winemakers.wine.dto.WineHomePageDto;
+import pl.klugeradoslaw.winemakers.wine.dto.WineSaveDto;
+import pl.klugeradoslaw.winemakers.storage.FileStorageService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class WineServiceImpl implements WineService {
     private final WineRepository wineRepository;
+    private final FileStorageService fileStorageService;
 
-    public WineServiceImpl(WineRepository wineRepository) {
+    public WineServiceImpl(WineRepository wineRepository, FileStorageService fileStorageService) {
         this.wineRepository = wineRepository;
+        this.fileStorageService = fileStorageService;
     }
 
     public List<WineHomePageDto> findAll() {
@@ -26,4 +32,19 @@ public class WineServiceImpl implements WineService {
         return wineRepository.findById(id).map(WineDtoMapper::mapFullResponse);
     }
 
+    @Override
+    public void addWine(WineSaveDto wineToSave) {
+        Wine wine = new Wine();
+        wine.setName(wineToSave.getName());
+        wine.setDateOfStart(wineToSave.getDateOfStart());
+        wine.setShortDescription(wineToSave.getShortDescription());
+        wine.setLongDescription(wineToSave.getLongDescription());
+        wine.setStatus(WineStatus.valueOf(wineToSave.getStatus()));
+        wine.setSteps(new ArrayList<Step>());
+        if (wineToSave.getPhoto() != null) {
+            String savedFileName = fileStorageService.saveImage(wineToSave.getPhoto());
+            wine.setPhoto(savedFileName);
+        }
+        wineRepository.save(wine);
+    }
 }
