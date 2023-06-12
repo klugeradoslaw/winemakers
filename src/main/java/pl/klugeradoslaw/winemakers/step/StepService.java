0e0fa881f2c30ede94377004651dfc85a21ef6d6
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-06-12 18:35:03 +0200

    added functionality of deleting one step in StepService, and endpoint in StepController

--- src/main/java/pl/klugeradoslaw/winemakers/step/StepService.java
+++ src/main/java/pl/klugeradoslaw/winemakers/step/StepService.java
@@ -3,17 +3,22 @@ package pl.klugeradoslaw.winemakers.step;
 import org.springframework.stereotype.Service;
 import pl.klugeradoslaw.winemakers.step.dto.StepFullResponseDto;
 import pl.klugeradoslaw.winemakers.step.dto.StepSaveDto;
+import pl.klugeradoslaw.winemakers.wine.Wine;
+import pl.klugeradoslaw.winemakers.wine.WineRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StepService {
 
     private final StepRepository stepRepository;
+    private final WineRepository wineRepository;
 
-    public StepService(StepRepository stepRepository) {
+    public StepService(StepRepository stepRepository, WineRepository wineRepository) {
         this.stepRepository = stepRepository;
+        this.wineRepository = wineRepository;
     }
 
     public List<StepFullResponseDto> findAll() {
@@ -35,6 +40,19 @@ public class StepService {
         List<Step> stepsToDelete = stepRepository.findByWineId(id);
         for (Step step : stepsToDelete) {
             stepRepository.delete(step);
+        }
+    }
+
+    //wyjatek do dodania
+    public void deleteStep(Long wineId, Long stepId) {
+        Optional<Wine> optionalWine = wineRepository.findById(wineId);
+        if (optionalWine.isPresent()) {
+            Wine wine = optionalWine.get();
+            List<Step> steps = wine.getSteps();
+            steps.removeIf(step -> step.getId().equals(stepId));
+            wineRepository.save(wine);
+        } else {
+            throw new RuntimeException();
         }
     }
 }
--- src/main/java/pl/klugeradoslaw/winemakers/web/StepController.java
+++ src/main/java/pl/klugeradoslaw/winemakers/web/StepController.java
@@ -2,6 +2,8 @@ package pl.klugeradoslaw.winemakers.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.klugeradoslaw.winemakers.common.Message;
@@ -25,6 +27,13 @@ public class StepController {
         stepService.addStep(id, step);
         model.addAttribute("step", step);
         model.addAttribute("message", new Message("Sukces!", "Dnia " + step.getDateOfStep() + " dodano kolejny etap wina o treści: " + step.getDescription()));
+        return "message";
+    }
+
+    @DeleteMapping("/wine/{wineId}/step/{stepId}/delete")
+    public String deleteStep(@PathVariable long wineId, @PathVariable long stepId, Model model) {
+        stepService.deleteStep(wineId, stepId);
+        model.addAttribute("message", new Message("Sukces!", "Usunięto krok."));
         return "message";
     }
 }
